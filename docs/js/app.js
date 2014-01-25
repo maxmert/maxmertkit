becce7e8df0b1e7a04aecad6f9543edd54d47e7c
Maxmert <vetrenko.maxim@gmail.com>
2014-01-25 21:59:16 +0400

    last

--- docs/js/app.js
+++ docs/js/app.js
@@ -119,3 +119,63 @@ app.config(function($routeProvider, $locationProvider) {
 });
 ;
 //# sourceMappingURL=app.js.map
+;var app, paths;
+
+app = angular.module('docsApp', ['ngRoute']);
+
+paths = {
+  tmpl: '/templates'
+};
+
+app.controller('PartialsMenu', function($scope) {});
+
+app.directive('menu', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "" + paths.tmpl + "/common/menu.html",
+    link: function(scope, element, attrs) {
+      return scope.items = window['partials'];
+    }
+  };
+});
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: "" + paths.tmpl + "/index.html"
+  }).otherwise({
+    templateUrl: "" + paths.tmpl + "/404.html"
+  });
+  return $locationProvider.html5Mode(true);
+});
+;
+//# sourceMappingURL=app.js.map
+;var app, paths;
+
+app = angular.module('docsApp', ['ngRoute']);
+
+paths = {
+  tmpl: '/templates'
+};
+
+app.controller('PartialsMenu', function($scope) {});
+
+app.directive('menu', function() {
+  return {
+    restrict: 'E',
+    templateUrl: "" + paths.tmpl + "/common/menu.html",
+    link: function(scope, element, attrs) {
+      return scope.items = window['partials'];
+    }
+  };
+});
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: "" + paths.tmpl + "/index.html"
+  }).otherwise({
+    templateUrl: "" + paths.tmpl + "/404.html"
+  });
+  return $locationProvider.html5Mode(true);
+});
+;
+//# sourceMappingURL=app.js.map
